fc15fe125a0858157e9674d1e9aab1326db2dcee
David Nanjila <nanjiladavid2@gmail.com>
2024-10-04 23:04:13 +0300

    Feat: Create a simple project that calculates the area of circle and a docker file to create its image
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# Docker Projects
+- To work with this projects, make sure that you have docker installed in your machine
---
+++ calculateArea/compute.js
@@ -0,0 +1,8 @@
+var radius = 7;
+var area = Math.pow(radius, 2) * Math.PI;
+console.log(
+    `The area of a circle with radius ${radius} cm 
+    is ${area} square cm`
+
+
+)
